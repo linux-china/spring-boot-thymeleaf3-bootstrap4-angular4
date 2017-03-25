4b3e72433afc32f89084ab728a21c6f37dc08493
linux_china <linux_china@hotmail.com>
2017-03-25 12:28:44 +0800

    Demo app test

---
+++ src/test/java/org/mvnsearch/web/DemoAppTests.kt
@@ -0,0 +1,21 @@
+package org.mvnsearch.web
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+/**
+ * Demo app test
+ *
+ * @author linux_china
+ */
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DemoAppTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
