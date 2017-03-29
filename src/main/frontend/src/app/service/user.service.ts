e3749384fdd31a8c94dd61b8f88da9df246364ed
linux_china <linux_china@hotmail.com>
2017-03-29 14:11:50 +0800

    inject http

--- src/main/frontend/src/app/service/user.service.ts
+++ src/main/frontend/src/app/service/user.service.ts
@@ -1,7 +1,11 @@
 import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
 
 @Injectable()
 export class UserService {
+
+    constructor(private http: Http) {
+    }
 
     findById(id: number): string {
         return "nick"
